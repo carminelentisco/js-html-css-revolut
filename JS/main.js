11b0fd93759bc374d8047e44692cff14dfd832fd
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-14 16:47:27 +0200

    JS
    Visibilità sotto menù

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,16 @@
 jQuery(document).ready( function($) {
 
     console.log('JS');
+    var dropDown = $('.menu-list-right .with-dropdown');
+    var dropLink = dropDown.children('a'); // link menù
+    var subMenu = dropDown.children('.submenu'); // Sotto menù
 
-});
+    dropLink.click(function () { 
+        
+        var actualMenu = $(this).next('.submenu');
+
+        actualMenu.toggle();
+
+    });
+
+}); // <--- End Page
